3e2ae985436d5a4cafad41ed5326ed597b4790c6
NALee <hieult1@smartosc.com>
2019-10-18 10:44:32 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  name: 'App'
+})
+export default class App extends Vue {}
+</script>
